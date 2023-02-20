2b60027e69f9633acd8d4667a429e646f9752a74
ilia <pocha000@mail.ru>
2023-02-20 18:30:50 +0300

    nutrition amount fix between 10 and 400 ml, empty list showing was fixed (month, week report)

--- dispatcher/src/main/java/ru/runner/repositories/foodCalculation/FoodCalculationService.java
+++ dispatcher/src/main/java/ru/runner/repositories/foodCalculation/FoodCalculationService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.runner.controller.TelegramBotOriginal;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -16,7 +15,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -193,11 +191,15 @@ public class FoodCalculationService {
                     dates.stream()
                             .filter(x -> x.getFeedTime().isAfter(startDate) && x.getFeedTime().isBefore(finishDate))
                             .collect(Collectors.toList());
+            if(weekList.isEmpty()){
+                return YOU_DO_NOT_HAVE_ENTRIES_MESSAGE;
+            }
 
             List<LocalDate> lastWeekDates = weekList.stream().map(x -> x.getFeedTime().toLocalDate()).distinct().collect(Collectors.toList());
 
             String weekOrMonth = daysToShow == 7 ? "Записи за последнюю неделю:\n" : "Записи за последний месяц:\n";
             sb.append(weekOrMonth);
+
             //TODO: реализовать поиск по введенному количеству дней
 
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMMM yyyy");
@@ -213,6 +215,8 @@ public class FoodCalculationService {
                     }
                 }
                 String times = String.valueOf(timesInt).matches("\\d*[234]") ? "раза" : "раз";
+
+
 
                 sb.append(lwd.format(dtf)).append(" : ").append(amount).append(" мл, ").append(timesInt).append(" ").append(times).append("\n");
             }
